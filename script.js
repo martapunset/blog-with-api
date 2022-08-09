2fd729514244b24b11c5b4ffe716cb7c335674e1
Marta Punset <martapunset10@gmail.com>
2022-08-09 17:46:55 +0200

    bootstrap first trial

--- script.js
+++ script.js
@@ -7,4 +7,9 @@
   http://localhost:3000
   */
 
-  
+  var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
